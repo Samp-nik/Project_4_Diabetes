df038667d34b220ca48fbff8f6925b92910653a8
lizmaxiel <135934423+lizmaxiel@users.noreply.github.com>
2023-12-05 20:34:35 -0500

    update

---
+++ Diabetes/Resources/Diabetes_Indicator_db.sql
@@ -0,0 +1,26 @@
+CREATE TABLE IF NOT EXISTS diabetes_binary_health_indicators_BRFSS2015 (
+    Diabetes_binary NUMERIC,
+    HighBP NUMERIC,
+    HighChol NUMERIC,
+    CholCheck NUMERIC,
+    BMI NUMERIC,
+    Smoker NUMERIC,
+    Stroke NUMERIC,
+    HeartDiseaseorAttack NUMERIC,
+    PhysActivity NUMERIC,
+    Fruits NUMERIC,
+    Veggies NUMERIC,
+    HvyAlcoholConsump NUMERIC,
+    AnyHealthcare NUMERIC,
+    NoDocbcCost NUMERIC,
+    GenHlth NUMERIC,
+    MentHlth NUMERIC,
+    PhysHlth NUMERIC,
+    DiffWalk NUMERIC,
+    Sex NUMERIC,
+    Age NUMERIC,
+    Education NUMERIC,
+    Income NUMERIC
+);
+
+SELECT * FROM diabetes_binary_health_indicators_BRFSS2015;
